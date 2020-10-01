0d370b83d726937bfaa50d6601b8fee1170b0223
Will Gittens <contato@trabalheseusonho.com.br>
2020-10-01 14:17:39 +0200

    Version 2.0.0
    New features: 
    - Create square thumbnails
    - A better error control, checking if the necessary libraries are installed.

--- superLoadSEO.php
+++ superLoadSEO.php
@@ -7,7 +7,7 @@
 	* @package   SuperLoadSEO
 	* @copyright Copyright (c) 2020 Betridee - Will Gittens ( https://betridee.com )
 	* @license   http://opensource.org/licenses/mit-license The MIT License
-	* @version   1.0.0
+	* @version   2.0.0
 	*/
 
     class superLoadSEO{
@@ -32,71 +32,181 @@
 
             }else{
 
-                $result["extension"] = false;
-                return $result;
+                $file_header = get_headers( $url , 1 );
+
+                if( isset( $file_header["Content-Type"] ) ){
+
+                    if( strpos( $file_header["Content-Type"], "text/css" )  !== false ){
+
+                        $result["extension"] = "css";
+                        $result["binary"] = $this->loadFile( $url );
+                        return $result;
+
+                    }     
+
+                    else if( strpos( $file_header["Content-Type"], "text/js" )  !== false ){
+
+                        $result["extension"] = "js";
+                        $result["binary"] = $this->loadFile( $url );
+                        return $result;
+
+                    }    
+
+                    else{
+
+                        die("<h3>Error:</h3>We could not find the file extension!");
+
+                    }                                
+
+                }else{
+
+                    die("<h3>Error:</h3>We could not find the file extension!");
+
+                }
 
             }
 
         }
 
-        public function createFile( $url , $maxSize = NULL , $quality = 50 , $cache = 800000, $lastmodified = 1577872800, $webp = 0 ){
+        private function libraryCheck(){
+
+            if( function_exists( "gd_info" ) ){
+
+                $compatibility = gd_info();
+                $result["jpg"] = $compatibility["JPEG Support"];
+                $result["png"] = $compatibility["PNG Support"];
+                $result["webp"] = $compatibility["WebP Support"];
+
+                return $result;
+
+            }else{
+
+                return false;
+
+            }
+
+        }
+
+        public function createFile( $url , $maxSize = NULL , $square = 0 , $quality = 50 , $cache = 800000, $lastmodified = 1577872800, $webp = 0 ){
 
             $file = $this->fileFormat( $url );
             $lastmodified = date( "D, d M Y H:i:s" , 1577872800 )." GMT";
 
             if( $file["extension"] == "jpg" || $file["extension"] == "jpeg" || $file["extension"] == "png" ){
 
-                if( $maxSize !== NULL ){
+                $support = $this->libraryCheck();
 
-                    $size = getimagesize( $url );
-                    $newPercentWidth = ( $maxSize * 100 ) / $size[0];
-                    $newHeight = round(( $size[1] * $newPercentWidth ) / 100);
+                if( $support !== false ){
 
-                    $loadImg = imagecreatefromstring( $file["binary"] );
-                    $createImg = imagecreatetruecolor( $maxSize , $newHeight );
+                    if( $maxSize !== NULL ){
 
-                    if( $file["extension"] == "png" ){
+                        $size = getimagesize( $url );
+                        $newPercentWidth = ( $maxSize * 100 ) / $size[0];
+                        $newHeight = round(( $size[1] * $newPercentWidth ) / 100);
 
-                        imagealphablending( $createImg, false );
-                        imagesavealpha( $createImg, true ); 
+                        $loadImg = imagecreatefromstring( $file["binary"] );
+                        
+                        if( $square == 1 ){
 
-                    }
+                            $createImg = imagecreatetruecolor( $maxSize , $maxSize );
 
-                    imagecopyresized( $createImg , $loadImg , 0 , 0 , 0 , 0 , $maxSize , $newHeight , $size[0] , $size[1] );                        
+                        }else{
 
-                    if( $file["extension"] == "png" && $webp == 0 ){
+                            $createImg = imagecreatetruecolor( $maxSize , $newHeight );
 
-                        header('Content-Type: image/png');
-                        header('Cache-Control: max-age='.$cache);
-                        header('Last-Modified: '.$lastmodified);
-                        imagepng( $createImg , NULL , 2 );
-                        imagedestroy( $createImg );
+                        }
 
-                    }
-                    
-                    else if( ( $file["extension"] == "jpg" || $file["extension"] == "jpeg" ) && $webp == 0 ){
+                        if( $file["extension"] == "png" ){
 
-                        header('Content-Type: image/jpeg');
-                        header('Cache-Control: max-age='.$cache);
-                        header('Last-Modified: '.$lastmodified);
-                        imagejpeg( $createImg , NULL , $quality );
-                        imagedestroy( $createImg );
+                            imagealphablending( $createImg, false );
+                            imagesavealpha( $createImg, true ); 
 
-                    }
-                    
-                    else if( $webp == 1 ){
+                        }
 
-                        header('Content-Type: image/webp');
-                        header('Cache-Control: max-age='.$cache);
-                        header('Last-Modified: '.$lastmodified);
-                        imagewebp( $createImg , NULL , $quality );
-                        imagedestroy( $createImg ); 
+                        if( $square == 1 ){
+
+                            if( $size[1] > $size[0] ){
+
+                                imagecopyresized( $createImg , $loadImg , 0 , 0 , 0 , 0 , $maxSize , $newHeight , $size[0] , $size[1] ); 
+
+                            }else{
+
+                                $diference = $maxSize - $newHeight;
+                                $positionX = - ( $diference / 2 );
+
+                                imagecopyresized( $createImg , $loadImg , $positionX , 0 , 0 , 0 , $maxSize + $diference , $newHeight + $diference , $size[0] , $size[1] ); 
+
+                            }
+
+                        }else{
+
+                            imagecopyresized( $createImg , $loadImg , 0 , 0 , 0 , 0 , $maxSize , $newHeight , $size[0] , $size[1] );
+
+                        }                      
+
+                        if( $file["extension"] == "png" && $webp == 0 ){
+
+                            if( $support["png"] == true ){
+
+                                header('Content-Type: image/png');
+                                header('Cache-Control: max-age='.$cache);
+                                header('Last-Modified: '.$lastmodified);
+                                imagepng( $createImg , NULL , 2 );
+                                imagedestroy( $createImg );
+
+                            }else{
+
+                                die("<h3>Error:</h3>PNG image support is not installed in your GD library.");
+
+                            }
+
+                        }
+                        
+                        else if( ( $file["extension"] == "jpg" || $file["extension"] == "jpeg" ) && $webp == 0 ){
+
+                            if( $support["jpg"] == true ){
+
+                                header('Content-Type: image/jpeg');
+                                header('Cache-Control: max-age='.$cache);
+                                header('Last-Modified: '.$lastmodified);
+                                imagejpeg( $createImg , NULL , $quality );
+                                imagedestroy( $createImg );
+
+                            }else{
+
+                                die("<h3>Error:</h3>JPG / JPEG image support is not installed in your GD library.");
+                                
+                            }
+
+                        }
+                        
+                        else if( $webp == 1 ){
+
+                            if( $support["webp"] == true ){
+
+                                header('Content-Type: image/webp');
+                                header('Cache-Control: max-age='.$cache);
+                                header('Last-Modified: '.$lastmodified);
+                                imagewebp( $createImg , NULL , $quality );
+                                imagedestroy( $createImg ); 
+
+                            }else{
+
+                                die("<h3>Error:</h3>WEBP image support is not installed in your GD library.");
+                                
+                            }
+
+                        }
+
+                    }else{
+
+                        die("<h3>Error:</h3>You need to inform the size of the new image when you call the function.<br><i><b>Send the variable GET 'size' in the url call.</b></i>");
 
                     }
 
                 }else{
 
-                    die("<h3>Error:</h3>You need to inform the size of the new image when you call the function.<br><i><b>Send the variable GET 'size' in the url call.</b></i>");
+                    die("<h3>Error:</h3>You apparently do not have the GD library installed on your server!");
 
                 }
 
@@ -127,31 +237,7 @@
                 header('Last-Modified: '.$lastmodified);
                 echo $file["binary"];
 
-            }
-
-            else if( $file["extension"] == false ){
-
-                $file_type = get_headers( $url , 1 );
-
-                if( strpos( $file_type["Content-Type"], "text/css" )  !== false  ){
-
-                        header('Content-Type: text/css');
-                        header('Cache-Control: max-age='.$cache);
-                        header('Last-Modified: '.$lastmodified);
-                        echo @file_get_contents( $url );
-
-                }
-
-                else if( strpos( $file_type["Content-Type"], "text/js" )  !== false  ){
-
-                        header('Content-Type: text/js');
-                        header('Cache-Control: max-age='.$cache);
-                        header('Last-Modified: '.$lastmodified);
-                        echo @file_get_contents( $url );
-
-                }                
-
-            }                    
+            }                  
 
         }
 
@@ -162,13 +248,14 @@
         if( $_GET["url"] !== ""  ){
 
             if( isset( $_GET["size"] ) ){ $var_size = $_GET["size"]; }else{ $var_size = NULL; }
+            if( isset( $_GET["square"] ) ){ $var_square = 1; }else{ $var_square = 0; }
             if( isset( $_GET["quality"] ) ){ $var_quality = $_GET["quality"]; }else{ $var_quality = 50; }
             if( isset( $_GET["cache"] ) ){ $var_cache = $_GET["cache"]; }else{ $var_cache = 800000; }
             if( isset( $_GET["lastmodified"] ) ){ $var_lmodif = $_GET["lastmodified"]; }else{ $var_lmodif = 1577872800; }
             if( isset( $_GET["webp"] ) ){ $var_webp = $_GET["webp"]; }else{ $var_webp = 0; }                                    
 
             $superLoadSEO = new superLoadSEO();
-            $superLoadSEO->createFile( urldecode($_GET["url"]) , $var_size , $var_quality , $var_cache , $var_lmodif , $var_webp );
+            $superLoadSEO->createFile( urldecode($_GET["url"]) , $var_size , $var_square , $var_quality , $var_cache , $var_lmodif , $var_webp );
 
         }else{
 
